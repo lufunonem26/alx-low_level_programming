0661534ea9b4623017dc02a0717f3ab084975f5c
Lufuno <Lufuno.nemudzivhadi26@outlook.com>
2022-06-02 08:00:27 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - program entry point
- *   * Return: 0
- *    */
+* main - program entry point
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
