8f9785c2a9cd1764bf45adfbfab58334b3616ac7
Lufuno <Lufuno.nemudzivhadi26@outlook.com>
2022-06-21 01:41:34 -0700

    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_chessboard - prints chessboard.
- * @a: matrix
-*/
-
+ * print_chessboard - Function prototype
+ * Description: Prints the chessboard
+ * @a: A pointer to a pointer to a char
+ * Return: void
+ */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8; j++)
+		int x;
+
+		for (x = 0; x < 8; x++)
 		{
-			_putchar(*(*(i + a) + j));
+			_putchar(*(*(i + a) + x));
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
